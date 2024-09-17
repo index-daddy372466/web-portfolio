4b05b19621094b7124b9dfa31db05a2f9fe2887f
KylesTech95 <108553476+KylesTech95@users.noreply.github.com>
2024-09-17 19:22:00 -0400

    fork: user experience update

--- src/lib/triggers/events/scroll.js
+++ src/lib/triggers/events/scroll.js
@@ -3,11 +3,15 @@ const workCon = document.getElementById('work')
 const disableScroll = () => {
     workCon.classList.remove('yes-scroll')
     workCon.classList.add('no-scroll')
+    document.body.classList.remove('yes-scroll')
+    document.body.classList.add('no-scroll')
 }
 // enable sroll for main content
 const enableScroll = () => {
     workCon.classList.remove('no-scroll')
     workCon.classList.add('yes-scroll')
+    document.body.classList.add('yes-scroll')
+    document.body.classList.remove('no-scroll')
 }
 
 
