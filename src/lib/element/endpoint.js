bd451c6faacf1db208b70f47e4d74359abb1d77b
kylestech95 <kylestech95@penguin>
2024-09-19 18:08:01 -0400

    feat: end-point-carosel

--- src/lib/element/endpoint.js
+++ src/lib/element/endpoint.js
@@ -1,36 +1,68 @@
-const endpointContainer = document.querySelector('.end-point-container')
-const edpointImg = document.querySelector('.end-point-img')
-const fw = document.querySelectorAll('.forward')
-const pv = document.querySelectorAll('.prev')
-console.log(fw)    
-console.log(pv)    
+const endpointContainer = document.querySelector(".end-point-container");
+const endpointImg = document.querySelector(".end-point-img");
+const article = endpointImg.parentElement;
+const fw = document.querySelectorAll(".forward");
+const pv = document.querySelectorAll(".prev");
 
-let media
-module.exports = endpoint = async() => {
+const stunPointer = () => {
+    let fingers = [...fw,...pv]
+  fingers.forEach((f) => {
+    f.classList.add("no-pointer");
+  });
+  setTimeout(() => {
+    fingers.forEach((f) => {
+      f.classList.remove("no-pointer");
+    });
+  }, 500);
+};
+module.exports = endpoint = async () => {
+  // fetch media endpoint
+  let files = await fetch("/api/media")
+    .then((r) => r.json())
+    .then((d) => [...d.media]);
 
-    console.log(endpointContainer)
-    media = await fetch('/api/media').then(r=>r.json()).then(d=>{
-        let files = [...d.media]
-        let target, src;
-        for( target = 0; target < files.length; target++){
-            fw.forEach(el=>{
-                el.onclick=e=>{
-                    (target+=1)
-                    target%=files.length;
-                    src = files[target]
-                    edpointImg.src = `./media/${src}`
-                }
-            })
-            
-            pv.forEach(el=>{
-                el.onclick=e=>{
-                    (target-=1)
-                    if(target < 0)target = files.length-1
-                    src = files[target]
-                    edpointImg.src = `./media/${src}`
-                }
-            })
-        }
+  let target,
+    src,
+    idx = Math.floor(Math.random() * files.length);
+  let startSrc = "./media/" + files[idx];
+  endpointImg.src = startSrc;
+  article.href = files[idx];
 
-    })
-}
+  for (target = 0; target < files.length; target++) {
+    if (target == idx) {
+      fw.forEach((el) => {
+        el.onclick = (e) => {
+          endpointImg.parentElement.classList.remove("left-to-right");
+          endpointImg.parentElement.classList.remove("right-to-left");
+          setTimeout(() => {
+            idx += 1;
+            idx %= files.length;
+            src = files[idx];
+            endpointImg.src = `./media/${src}`;
+            endpointImg.parentElement.classList.add("left-to-right");
+            article.href = files[idx];
+            console.log(idx);
+            stunPointer();
+          }, 100);
+        };
+      });
+
+      pv.forEach((el) => {
+        el.onclick = (e) => {
+          endpointImg.parentElement.classList.remove("left-to-right");
+          endpointImg.parentElement.classList.remove("right-to-left");
+          setTimeout(() => {
+            idx -= 1;
+            if (idx < 0) idx = files.length - 1;
+            src = files[idx];
+            endpointImg.src = `./media/${src}`;
+            endpointImg.parentElement.classList.add("right-to-left");
+            article.href = files[idx];
+            console.log(idx);
+            stunPointer();
+          }, 100);
+        };
+      });
+    }
+  }
+};
