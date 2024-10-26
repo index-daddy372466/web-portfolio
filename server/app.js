080043c5fae5f0f193c586841d416e774bd48722
KylesTech95 <108553476+KylesTech95@users.noreply.github.com>
2024-10-26 15:20:57 -0400

    fork: title for sql

--- server/app.js
+++ server/app.js
@@ -19,21 +19,21 @@ app.use(cookieParser());
 app.use((req, res, next) => {
   const bytes = crypto.randomBytes(32)
   const salt = crypto.randomBytes(16).toString('hex')
-  console.log('bytes')
+  // console.log('bytes')
   // console.log(bytes)
   // console.log(bytes.toString('hex'))
   const hashed = crypto.scryptSync(bytes,salt,64).toString('hex')
   const newBytes = `${salt}:${hashed}`
-  console.log(hashed)
-  console.log(newBytes)
+  // console.log(hashed)
+  // console.log(newBytes)
 
   res.locals.nonce = crypto
     .createHash("sha256", newBytes)
     .update(newBytes)
     .digest("hex");
-    console.log("")
-    console.log('local nonce')
-    console.log(res.locals.nonce)
+    // console.log("")
+    // console.log('local nonce')
+    // console.log(res.locals.nonce)
   next();
 });
 app.use(function(req, res, next) {
