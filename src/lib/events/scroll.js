58a3d9dadc8f33209a2d0e9d556b00feb7e54158
KylesTech95 <108553476+KylesTech95@users.noreply.github.com>
2024-10-30 10:56:42 -0400

    feat: manipulate footer access

--- src/lib/events/scroll.js
+++ src/lib/events/scroll.js
@@ -5,19 +5,35 @@ const limit = 150;
 const dbdshr = document.querySelector(".dbds-hr");
 const midscreen = window.innerHeight / 2;
 dbdshr.style.top = midscreen + "px";
+const footer = document.getElementById("contact");
 
 // elongate work-container section on scroll
 const listenScroll = (e) => {
+  console.log('cl height')
+  console.log(e.target.clientHeight)
+  console.log(e.target.scrollTop)
   if (e.target.scrollTop >= limit) {
     banner.classList.remove("banner-reg");
     banner.classList.add("banner-sm");
     navPos = banner.clientHeight;
     nav.style.top = navPos + "px";
-  } else {
+    footer.classList.remove('footer-up') 
+    footer.classList.add('footer-down') 
+  } 
+  else {
     banner.classList.add("banner-reg");
     banner.classList.remove("banner-sm");
     navPos = banner.clientHeight;
     nav.style.top = navPos + "px";
+    footer.classList.remove('footer-down') 
+    footer.classList.add('footer-up') 
+  }
+
+
+  if((e.target.scrollTop) >= e.target.clientHeight){
+    console.log('you hit the target bro on scroll')
+    footer.classList.remove('footer-down') 
+    footer.classList.add('footer-up') 
   }
 };
 
