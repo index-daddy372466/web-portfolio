fe91c934a6deea4aef412c1451a72286f5024569
testmailbox987654 <testmailbox987654@penguin>
2024-10-30 06:47:00 -0400

    fork: stage language tags based on underdev status

--- src/lib/element/tags.js
+++ src/lib/element/tags.js
@@ -1,22 +1,37 @@
 
 module.exports = loadTags = ([...captions]) => {
    let fcaptions = document.querySelectorAll('figcaption.f-caption')
-   console.log(captions)
+   let figures = document.querySelectorAll('figure')
    let tagText = 'under development'
+
+   // filter captons by h3 tags
    fcaptions = [...fcaptions].filter(cap=>{
-      // console.log(cap)
       const text = cap.textContent.toLowerCase();
       let capincluded = captions.includes(text);
-      // if(capincluded)order.push(text)
       return capincluded
    })
-   console.log(fcaptions)
+   
+   // figure captions
    fcaptions.forEach(caption=>{
       let figure = caption.parentElement;
       let h3 = document.createElement('h3')
       h3.classList.add('tag-h3')
       h3.textContent = tagText;
       figure.append(h3)
+   })
+
+   // language tags
+   figures.forEach((fig,newton)=>{
+   const article = fig.children[0];
+   let icons = [...article.children].filter(x=>/fa-/i.test(x.classList))
+   let h3 = [...fig.children].findIndex(x=>/tag-h3/i.test(x.classList))
+   icons.reduce((a,b)=>{
+      if(h3==-1){
+         b.classList.add('icon-top')
+      } else {
+         b.classList.add('icon-bottom')
+      }
+   },0)
    })
    
 }
