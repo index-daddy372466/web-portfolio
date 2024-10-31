0e53c2c5f0f7752ce17834b31909dfeae9d2aaca
KylesTech95 <108553476+KylesTech95@users.noreply.github.com>
2024-10-31 18:15:14 -0400

    fork: webpack mode

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const CssPlug = require("mini-css-extract-plugin");
 
 module.exports = {
   // mode
-  mode: (!process.env.TYPE || process.env.TYPE!='production') ? 'development' : process.env._TYPE_,
+  mode: (!process.env.TYPE || process.env.TYPE!='production') ? 'development' : process.env.TYPE,
   // entry
   entry: [
     path.resolve(__dirname, "src/londonTower.js"),
